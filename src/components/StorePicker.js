94459fb2aad089dedc24c8184a3066936f852ec2
Tom Gillard <tomgillar@gmail.com>
2017-08-31 16:39:48 +1000

    Adding function to get store name and navigate to app page

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -4,15 +4,31 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+  // constructor() {
+  //   super();
+  //
+  //   this.goToStore = this.goToStore.bind(this);
+  // }
+
+  goToStore(event) {
+    event.preventDefault();
+    console.log(this.storeInput);
+    // grab text from box
+    // const store = this.storeInput;
+    // navigate to store page
+    // window.location = `/store/${store}`;
+  }
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={(e) => this.goToStore(e)}>
         <h2>Please enter a store</h2>
-        <input type="text" placeholder="Store Name" defaultValue={getFunName()} required />
+        <input type="text" placeholder="Store Name" defaultValue={getFunName()} ref={(input) => { this.storeInput = input }} required />
         <button type="submit">Visit Store</button>
       </form>
     );
   }
+
 };
 
 export default StorePicker;
