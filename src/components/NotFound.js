ea90984d0e25f9bd1ea626d83052686dc6dbd470
Tom Gillard <tomgillar@gmail.com>
2017-08-31 15:41:16 +1000

    Adding routing to index file along with 404 component

---
+++ src/components/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>Page Not found</h1>
+      <p>Looks like this one got away!</p>
+    </div>
+  );
+};
+
+export default NotFound;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // App Libs
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter, Match, Miss } from 'react-router';
 
 // App Styles
 import './css/style.css';
@@ -8,6 +9,19 @@ import './css/style.css';
 // App Components
 import App from './components/App';
 import StorePicker from './components/StorePicker';
+import NotFound from './components/NotFound';
+
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Match exactly pattern="/" component={StorePicker} />
+        <Match pattern="/store/:storeId" component={App} />
+        <Miss component={NotFound} />
+      </div>
+    </BrowserRouter>
+  );
+}
 
 // Hook it up to index.html
-render(<App />, document.querySelector('#main'));
+render(<Root />, document.querySelector('#main'));
