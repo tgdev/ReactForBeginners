c3da0cd48d3d339ca67bdd882f8a5f95e8a8946d
Tom Gillard <tomgillar@gmail.com>
2017-08-31 16:25:30 +1000

    Adding helper to store picker component to help generate random store name

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
+// Helpers
+import { getFunName } from '../helpers';
+
 class StorePicker extends React.Component {
   render() {
     return (
       <form className="store-selector">
         <h2>Please enter a store</h2>
-        <input type="text" placeholder="Store Name" required />
+        <input type="text" placeholder="Store Name" defaultValue={getFunName()} required />
         <button type="submit">Visit Store</button>
       </form>
     );
