745d740b7cdb40436766d427cd7d94b0234e1ba1
Tom Gillard <tomgillar@gmail.com>
2017-08-31 15:09:35 +1000

    Adding base store picker component to project and hooking into main div of index file

---
+++ src/components/StorePicker.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class StorePicker extends React.Component {
+  render() {
+    return (
+      <div>Store picker</div>
+    );
+  }
+};
+
+export default StorePicker;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-// let's go!
+// Libs
+import React from 'react';
+import { render } from 'react-dom';
+
+// Components
+import StorePicker from './components/StorePicker';
+
+// Hook it up to index.html
+render(<StorePicker />, document.querySelector('#main'));
